89ad960caf2583bbcd66d720af8695e3d0963440
Juan V Lopes <juan.versolato@outlook.com>
2020-02-15 07:58:46 -0400

    Put my own informations

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Develop Blog`,
+    description: `A blog about frontend development and other cool stuffs.`,
+    author: `Juan Versolato Lopes`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
